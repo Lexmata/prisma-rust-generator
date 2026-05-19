feb3fdff4db59fce8881dafa58265a557f1ef2e0
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2026-05-19 11:11:40 -0400

    chore: add eslint and vitest configs

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["src/**/*.ts", "test/**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: { project: "./tsconfig.json", ecmaVersion: 2022, sourceType: "module" },
+    },
+    plugins: { "@typescript-eslint": tseslint },
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "no-console": ["warn", { allow: ["error", "warn"] }],
+    },
+  },
+];
